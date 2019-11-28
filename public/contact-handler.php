1ecd8a6dfba4c80343ccd46175aa40053a5d762f
Emma <emmaborghult@gmail.com>
2019-11-28 22:22:41 +0100

    Change to swiftmailer for contact form

--- public/contact-handler.php
+++ public/contact-handler.php
@@ -1,16 +1,24 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+require_once '/path/to/vendor/autoload.php';
 
-    $mailTo = "emmaborghult@gmail.com";
-    $headers = "Från: ".$email;
-    $text = "Du har blivit kontaktad på din hemsida av ".$name.".\n\n".$message;
+// Create the Transport
+$transport = (new Swift_SmtpTransport('smtp.example.org', 25))
+  ->setUsername('your username')
+  ->setPassword('your password')
+;
 
-    mail($mailTo, $text, $headers);
-    header("Location: index.html");
-}
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
+
+// Create a message
+$message = (new Swift_Message('Wonderful Subject'))
+  ->setFrom(['john@doe.com' => 'John Doe'])
+  ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
+  ->setBody('Here is the message itself')
+  ;
+
+// Send the message
+$result = $mailer->send($message);
 
 ?>
