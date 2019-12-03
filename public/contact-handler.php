988be923bbf82f4475a12b4ee583c196a2c32289
Emma <emmaborghult@gmail.com>
2019-12-03 20:09:12 +0100

    Install SwiftMailer + add contact-handler code

--- public/contact-handler.php
+++ public/contact-handler.php
@@ -1,24 +1,29 @@
 <?php
+ if (isset($_POST['submit'])){
+    require_once '/path/to/vendor/autoload.php';
 
-require_once '/path/to/vendor/autoload.php';
-
-// Create the Transport
-$transport = (new Swift_SmtpTransport('smtp.example.org', 25))
-  ->setUsername('your username')
-  ->setPassword('your password')
-;
-
-// Create the Mailer using your created Transport
-$mailer = new Swift_Mailer($transport);
-
-// Create a message
-$message = (new Swift_Message('Wonderful Subject'))
-  ->setFrom(['john@doe.com' => 'John Doe'])
-  ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
-  ->setBody('Here is the message itself')
-  ;
-
-// Send the message
-$result = $mailer->send($message);
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    // Create the Transport
+    $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+      ->setUsername('emmaborghult@gmail.com')
+      ->setPassword('flingan95')
+    ;
+    
+    // Create the Mailer using your created Transport
+    $mailer = new Swift_Mailer($transport);
+    
+    // Create a message
+    $msg = (new Swift_Message('Mail från hemsidan'))
+      ->setFrom([$email => $name])
+      ->setTo(['emmaborghult@gmail.com'])
+      ->setBody('Namn: $name.Email: $email.Meddelande: $message');
+    
+    // Send the message
+    $result = $mailer->send($msg);
+  
+ }
 
 ?>
