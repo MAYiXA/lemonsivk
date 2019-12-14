eecc4d1f38f588a712962b2df2ce4ef44680e5c6
Emma <emmaborghult@gmail.com>
2019-12-14 19:17:31 +0100

    Make toggle menu function with JS

--- public/app.js
+++ public/app.js
@@ -1,12 +1,41 @@
-const toggle = document.querySelector('.toggle-bars');
-const menuExpanded = document.querySelector('.menu-expanded');
+const toggleBtn = document.querySelector('.toggle-bars');
+const menu = document.querySelector('.menu-expanded');
+const menuLink = document.querySelectorAll('.expand-link');
 
-toggle.innerHTML = '<i class="fas fa-bars"></i>';
+toggleBtn.innerHTML = '<i class="fas fa-bars"></i> <i class="fas fa-times"></i>';
+const bars = document.querySelector('.fa-bars');
+const x = document.querySelector('.fa-times');
+x.style.display = 'none';
 
-toggle.addEventListener('click', () => {
-    if (menuExpanded === true) {
-        menuExpanded.classList.add('menu-expanded');
+toggleMenu = () => {
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
     } else {
-        menuExpanded.classList.remove('menu-expanded');
+        menu.style.display = 'block';
     }
-})
+};
+
+iconChange = () => {
+    if (bars.style.display === 'none') {
+        bars.style.display = 'block';
+    } else {
+        bars.style.display = 'none';
+    }
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+toggleBtn.addEventListener('click', () => {
+    toggleMenu();
+    iconChange();
+});
+
+menuLink.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.style.display = 'none';
+        iconChange();
+    });
+});
